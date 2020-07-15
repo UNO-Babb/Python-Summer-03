e424c6e62a734442c922722d62337f5f94412b63
Derek Babb <dvbabb@unomaha.edu>
2020-07-15 11:02:03 -0500

    Updated turtle name.

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -14,16 +14,16 @@ def drawSquare(myTurtle, size):
 
 
 def main():
-    myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
+    leo = turtle.Turtle()
+    drawSquare(leo, 200)
+    # drawPolygon(leo, 5) #draws a pentagon
+    # drawPolygon(leo, 8) #draws an octogon
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    # fillCorner(leo, 2) #draws a square with top right corner filled in.
+    # fillCorner(leo, 3) #draws a square bottom left corner filled in.
 
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+    # squaresInSquares(leo, 5) #draws 5 concentric squares
+    # squaresInSquares(leo, 3) #draws 3 concentric squares
 
 
 main()
-input("Hit enter to quit") #this keeps the turtle window from disapearing too soon.
